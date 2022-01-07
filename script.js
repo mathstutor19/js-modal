226a65c2641d825fcce98721baebd3cac2ce0252
Farkhod <33034567+Farkhod007@users.noreply.github.com>
2022-01-07 20:07:21 +0500

    Make object based

--- script.js
+++ script.js
@@ -9,28 +9,73 @@
 //     console.log(btnsOpenModal[i].textContent);
 // }
 // 7.12 lesson working with class
+
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-const openModal = function() {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-}
-const closeModal = function() {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-}
-for (let i = 0; i < btnsOpenModal.length; i++) {
-    btnsOpenModal[i].addEventListener('click', openModal)
-}
-btnCloseModal.addEventListener('click', closeModal)
-overlay.addEventListener('click', closeModal)
 
-// 7.13 handling an esc keypress event 
-document.addEventListener('keydown', function(e) {
-    console.log(e.key);
-    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-        closeModal();
+// const openModal = function () {
+//     modal.classList.remove('hidden');
+//     overlay.classList.remove('hidden');
+// };
+
+// const closeModal = function () {
+//     modal.classList.add('hidden');
+//     overlay.classList.add('hidden');
+// };
+
+// for (let i = 0; i < btnsOpenModal.length; i++) {
+//     btnsOpenModal[i].addEventListener('click', openModal);
+// }
+
+// // [...btnsOpenModal].map(btn => btn.addEventListener('click', openModal));
+
+// btnCloseModal.addEventListener('click', closeModal);
+// overlay.addEventListener('click', closeModal);
+
+// // 7.13 handling an esc keypress event
+// document.addEventListener('keydown', function (e) {
+//     console.log(e.key);
+//     if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+//         closeModal();
+//     }
+// });
+
+class Modal {
+    constructor(modal, overlay, closeButton, openButtons) {
+        this.modal = modal;
+        this.overlay = overlay;
+        this.closeButton = closeButton;
+        this.openButtons = openButtons;
+        this.addEventListeners();
     }
-})
+
+    addEventListeners() {
+		for (let i = 0; i < this.openButtons.length; i++) {
+            this.openButtons[i].addEventListener('click', this.open.bind(this));
+        }
+
+		this.closeButton.addEventListener('click', this.close.bind(this));
+		this.overlay.addEventListener('click', this.close.bind(this));
+
+        document.addEventListener('keydown', (function (e) {
+            if (e.key === 'Escape' && !this.modal.classList.contains('hidden')) {
+                this.close();
+            }
+        }).bind(this));
+    }
+
+    open() {
+		console.log(this.modal);
+        this.modal.classList.remove('hidden');
+        this.overlay.classList.remove('hidden');
+    }
+
+    close() {
+        this.modal.classList.add('hidden');
+        this.overlay.classList.add('hidden');
+    }
+}
+
+let messageModal = new Modal(modal, overlay, btnCloseModal, btnsOpenModal);
